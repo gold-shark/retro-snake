f5873e16fbc98f9b1b11d082396fe5f08426f709
heqiong <376497829@qq.com>
2016-11-22 14:37:02 +0800

    snake class

--- js/snake.js
+++ js/snake.js
@@ -1,7 +1,124 @@
+(function() {
+  /* 点类 */
+  function Point(x, y) {
+    this.x = x;
+    this.y = y;
+    this.isExist = true;
+  };
+  /* 蛇类 */
+  function Snake(x, y, leg, speed) {
+    this.x = x;
+    this.y = y;
+    this.leg = leg || 3; //长度
+    this.speed = speed || 200; //速度
+    this.direction = Math.floor(Math.random() * 4) + 37; //随机分配一个方向
+    this.points = []; //点集
+    this.integral = 0; //积分
+    this.isOver = false; //是否结束
+    this.init();
+  };
+  /* 行动 */
+  Snake.prototype.action = function(point, border) {
+    var x = this.points[0].x;
+    var y = this.points[0].y;
+    switch(this.direction) {
+      case 37: //左
+        x--;
+        break;
+      case 38: //上
+        y--;
+        break;
+      case 39: //右
+        x++;
+        break;
+      case 40: //下
+        y++;
+        break;
+    }
+    var p = new Point(x, y);
+    if(this.isToBoundary(p, border) || this.isBite(p)) {
+      this.isOver = true;
+    } else {
+      if(this.isEat(point)) {
+        point.isExist = false;
+        this.integral += 10;
+      } else {
+        this.points.pop();
+      }
+      this.points.unshift(p);
+    }
+  };
+  /* 是否吃到东西 */
+  Snake.prototype.isEat = function(point) {
+    var b = false;
+    if(this.points[0].x === point.x && this.points[0].y === point.y) {
+      b = true;
+    }
+    return b;
+  };
+  /* 是否有重合 */
+  Snake.prototype.isBite = function(point) {
+    var b = false;
+    for(var i in this.points) {
+      if(this.points[i].x === point.x && this.points[i].y === point.y) {
+        b = true;
+      }
+    }
+    return b;
+  };
+  Snake.prototype.isToBoundary = function(point, border) {
+    var b = false;
+    if(point.x < border.x1 || point.x > border.x2 || point.y < border.y1 || point.y > border.y2) {
+      b = true;
+    }
+    return b;
+  };
+  /* 初始化 */
+  Snake.prototype.init = function() {
+    var points = [];
+    for(var i = 0; i < this.leg; i++) {
+      var p;
+      switch(this.direction) {
+        case 37: //左
+          p = new Point(this.x + i, this.y);
+          break;
+        case 38: //上
+          p = new Point(this.x, this.y + i);
+          break;
+        case 39: //右
+          p = new Point(this.x - i, this.y);
+          break;
+        case 40: //下
+          p = new Point(this.x, this.y - i);
+          break;
+      }
+      points.push(p);
+    }
+    this.points = points;
+  };
+  this.Snake = Snake;
+  this.Point = Point;
+}());
+
 $(function() {
-  var unit = 15;
   var c = document.getElementById('canvas');
   var cxt = c.getContext('2d');
+  var unit = 15;
+  var border = {
+    x1: 0,
+    y1: 0,
+    x2: c.width / unit - 1,
+    y2: c.height / unit - 1
+  };
+  var sx = Math.floor(Math.random() * (c.width / unit - 10)) + 5;
+  var sy = Math.floor(Math.random() * (c.height / unit - 10)) + 5;
+  var px = Math.floor(Math.random() * c.width / unit);
+  var py = Math.floor(Math.random() * c.height / unit);
+  var snake = new Snake(sx, sy);
+  var point = new Point(px, py);
+  var intervalObj;
+  var timeOutObj;
+  var time = snake.speed;
   /* 绘制背景  */
   function drawBackground() {
     cxt.beginPath();
@@ -38,97 +155,6 @@ $(function() {
       cxt.fillRect(snake.points[i].x * unit, snake.points[i].y * unit, unit, unit);
     }
   };
-  /* 点类 */
-  function Point(x, y) {
-    this.x = typeof x === 'undefined' ? Math.floor(Math.random() * c.width / unit) : x;
-    this.y = typeof y === 'undefined' ? Math.floor(Math.random() * c.height / unit) : y;
-    this.isExist = true;
-  };
-  /* 蛇类 */
-  function Snake(x, y, leg) {
-    this.x = typeof x === 'undefined' ? Math.floor(Math.random() * (c.width / unit - 10)) + 5 : x;
-    this.y = typeof y === 'undefined' ? Math.floor(Math.random() * (c.height / unit - 10)) + 5 : y;
-    this.leg = leg || 3;
-    this.points = []; //点集
-    this.direction = Math.ceil(Math.random() * 4) + 36;
-    this.integral = 0; //积分
-    this.isOver = false; //是否结束
-    this.speed = 200; //速度
-    /* 行动 */
-    this.action = function(point) {
-      var x = this.points[0].x;
-      var y = this.points[0].y;
-      switch(this.direction) {
-        case 37: //左
-          x--;
-          break;
-        case 38: //上
-          y--;
-          break;
-        case 39: //右
-          x++;
-          break;
-        case 40: //下
-          y++;
-          break;
-      }
-      var p = new Point(x, y);
-      if(x < 0 || x > (c.width / unit - 1) || y < 0 || y > (c.height / unit - 1) || this.isBite(p)) {
-        this.isOver = true;
-      } else {
-        if(this.eat(point)) {
-          point.isExist = false;
-          this.integral += 10;
-        } else {
-          this.points.pop();
-        }
-        this.points.unshift(p);
-      }
-      $('.integral').find('em').text(this.integral);
-    };
-    /* 是否吃到东西 */
-    this.eat = function(point) {
-      var b = false;
-      if(this.points[0].x === point.x && this.points[0].y === point.y) {
-        b = true;
-      }
-      return b;
-    };
-    /* 是否有重合 */
-    this.isBite = function(point) {
-      var b = false;
-      for(var i in this.points) {
-        if(this.points[i].x === point.x && this.points[i].y === point.y) {
-          b = true;
-        }
-      }
-      return b;
-    };
-    /* 初始化 */
-    this.init = function() {
-      var points = [];
-      for(var i = 0; i < this.leg; i++) {
-        var p;
-        switch(this.direction) {
-          case 37: //左
-            p = new Point(this.x + i, this.y);
-            break;
-          case 38: //上
-            p = new Point(this.x, this.y + i);
-            break;
-          case 39: //右
-            p = new Point(this.x - i, this.y);
-            break;
-          case 40: //下
-            p = new Point(this.x, this.y - i);
-            break;
-        }
-        points.push(p);
-      }
-      this.points = points;
-    };
-    this.init();
-  };
   /* 加速 */
   function accelerate(snake) {
     if(time > 50) {
@@ -141,9 +167,12 @@ $(function() {
   };
   /* 动画效果 */
   function anima() {
-    snake.action(point);
+    snake.action(point, border);
+    $('.integral').find('em').text(snake.integral);
     if(!point.isExist) {
-      point = new Point();
+      var px = Math.floor(Math.random() * c.width / unit);
+      var py = Math.floor(Math.random() * c.height / unit);
+      point = new Point(px, py);
     }
     accelerate(snake);
     clearDraw();
@@ -160,13 +189,7 @@ $(function() {
     drawSnake(snake);
     drawPoint(point);
     intervalObj = setInterval(anima, time);
-  }
-
-  var snake = new Snake();
-  var point = new Point();
-  var intervalObj;
-  var timeOutObj;
-  var time = snake.speed;
+  };
   init();
 
   $(document).on('keydown', fun = function(e) {
